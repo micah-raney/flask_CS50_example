33098e4434e7d2db1cf8c75f1f11d8920b78d734
Micah Raney <micah-raney@users.noreply.github.com>
2023-08-09 22:17:21 +1200

    add support for form data collection

--- froshims/app.py
+++ froshims/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__) # Turn this file into a Flask app
 
+registrants = {}
+
 # Python Decorator assigns the below function to the assigned route.
 @app.route("/")
 def index():
@@ -9,4 +11,7 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
+	name = request.form.get("name")
+	sport = request.form.get("sport")
+	registrants[name] = sport
 	return render_template("success.html")
