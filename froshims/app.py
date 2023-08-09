701abefb32cf4912d093eb4576b6b5dea63be83d
Micah Raney <micah-raney@users.noreply.github.com>
2023-08-09 22:33:26 +1200

    add supported sports list and dynamic selector

--- froshims/app.py
+++ froshims/app.py
@@ -4,10 +4,12 @@ app = Flask(__name__) # Turn this file into a Flask app
 
 registrants = {}
 
+sports = ["Basketball", "Soccer", "Ultimate Frisbee"]
+
 # Python Decorator assigns the below function to the assigned route.
 @app.route("/")
 def index():
-	return render_template("index.html")
+	return render_template("index.html", sports=sports)
 
 @app.route("/register", methods=["POST"])
 def register():
