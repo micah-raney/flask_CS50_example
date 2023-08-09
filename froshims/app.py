a5b59bb7da32b1faa910cea20439276e4dee4460
Micah Raney <micah-raney@users.noreply.github.com>
2023-08-09 22:11:16 +1200

    add registration form

--- froshims/app.py
+++ froshims/app.py
@@ -5,4 +5,8 @@ app = Flask(__name__) # Turn this file into a Flask app
 # Python Decorator assigns the below function to the assigned route.
 @app.route("/", methods=["GET", "POST"])
 def index():
-		return render_template("index.html")
+	return render_template("index.html")
+
+@app.route("/register")
+def register():
+	return render_template("success.html")
