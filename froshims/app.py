757b15ddd82a49b79e157ef1c173c74a16956217
Micah Raney <micah-raney@users.noreply.github.com>
2023-08-09 23:07:26 +1200

    make global vars all-caps

--- froshims/app.py
+++ froshims/app.py
@@ -2,25 +2,25 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__) # Turn this file into a Flask app
 
-registrants = {}
+REGISTRANTS = {}
 
-sports = ["Basketball", "Soccer", "Ultimate Frisbee"]
+SPORTS = ["Basketball", "Soccer", "Ultimate Frisbee"]
 
 # Python Decorator assigns the below function to the assigned route.
 @app.route("/")
 def index():
-	return render_template("index.html", sports=sports)
+	return render_template("index.html", sports=SPORTS)
 
 @app.route("/register", methods=["POST"])
 def register():
 	name = request.form.get("name")
 	sport = request.form.get("sport")
-	if ( (sport not in sports) or (not name) ):
+	if ( (sport not in SPORTS) or (not name) ):
 		return render_template("failure.html")
-	registrants[name] = sport
+	REGISTRANTS[name] = sport
 	return render_template("success.html")
 
 
 @app.route("/registrants")
 def registrant_page():
-	return render_template("registrants.html", registrants=registrants)
+	return render_template("registrants.html", registrants=REGISTRANTS)
