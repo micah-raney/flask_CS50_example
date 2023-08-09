6f2b0f7ac5efdf84f28a73ee42f431e2c36369ef
Micah Raney <micah-raney@users.noreply.github.com>
2023-08-09 22:12:50 +1200

    add POST method to form for register function

--- froshims/app.py
+++ froshims/app.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__) # Turn this file into a Flask app
 
 # Python Decorator assigns the below function to the assigned route.
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
 	return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
 	return render_template("success.html")
